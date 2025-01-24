74c1a8fcd8632a60d13083c3d2c9b25b5303476d
Jeremy Bernstein <14089157+jxbz@users.noreply.github.com>
2025-01-24 11:39:39 -0500

    switch to quintic ns

--- modula/atom.py
+++ modula/atom.py
@@ -3,11 +3,19 @@ import jax.numpy as jnp
 
 from modula.abstract import Atom
 
-def orthogonalize(m):
-    m = m / jnp.linalg.norm(m)
+def orthogonalize(M):
+    a, b, c = 3.0, -3.2, 1.2
+    transpose = M.shape[1] > M.shape[0]
+    if transpose:
+        M = M.T
+    M = M / jnp.linalg.norm(M)
     for _ in range(10):
-        m = 3/2 * m - 1/2 * m @ m.T @ m
-    return m
+        A = M.T @ M
+        I = jnp.eye(A.shape[0])
+        M = M @ (a * I + b * A + c * A @ A)
+    if transpose:
+        M = M.T
+    return M
 
 class Linear(Atom):
     def __init__(self, fanout, fanin):
@@ -43,3 +51,26 @@ class Linear(Atom):
         grad_weight = grad_w[0]
         d_weight = orthogonalize(grad_weight)
         return [d_weight * target_norm]
+
+if __name__ == "__main__":
+
+    key = jax.random.PRNGKey(0)
+
+    # sample a random d0xd1 matrix
+    d0, d1 = 50, 100
+    M = jax.random.normal(key, shape=(d0, d1))
+    O = orthogonalize(M)
+
+    # compute SVD of M and O
+    U, S, Vh = jnp.linalg.svd(M, full_matrices=False)
+    s = jnp.linalg.svd(O, compute_uv=False)
+
+    # print singular values
+    print(f"min singular value of O: {jnp.min(s)}")
+    print(f"max singular value of O: {jnp.max(s)}")
+
+    # check that M is close to its SVD
+    error_M = jnp.linalg.norm(M - U @ jnp.diag(S) @ Vh) / jnp.linalg.norm(M)
+    error_O = jnp.linalg.norm(O - U @ Vh) / jnp.linalg.norm(U @ Vh)
+    print(f"relative error in M's SVD: {error_M}")
+    print(f"relative error in O: {error_O}")
