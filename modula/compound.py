6d058d3d9ade1ae9694c75f9c92de9c21d210dae
Laker Newhouse <lakern@mit.edu>
2025-03-01 09:13:49 -0500

    Define GPT

--- modula/compound.py
+++ modula/compound.py
@@ -18,7 +18,16 @@ def Attention(num_heads, d_embed, d_query, d_value, softmax_scale, causal):
     return W @ 1/3 * AttentionOutput() @ (V, AttentionScores)
 
 def GPT(vocab_size, num_heads, d_embed, d_query, d_value, num_blocks, blocks_mass=5, softmax_scale=1.0):
-    # still needs to be implemented:
-    #   - RoPE
-    #   - putting the blocks together
-    pass
+    embed = Embed(d_embed, vocab_size)
+    embed.tare()
+
+    att = Attention(num_heads, d_embed, d_query, d_value, softmax_scale, causal=True)
+    mlp = Linear(d_embed, 4*d_embed) @ GeLU() @ Linear(4*d_embed, d_embed)
+    att_block = (1-1/(2*num_blocks)) * Identity() + 1/(2*num_blocks) * att
+    mlp_block = (1-1/(2*num_blocks)) * Identity() + 1/(2*num_blocks) * mlp
+    blocks = (mlp_block @ att_block) ** num_blocks
+    blocks.tare(absolute=blocks_mass)
+
+    out = Linear(vocab_size, d_embed)
+
+    return out @ blocks @ embedf
