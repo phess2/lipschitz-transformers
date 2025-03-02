788f2068b104e6923716731cfd463e64daa26c25
Laker Newhouse <lakern@mit.edu>
2025-03-02 12:20:12 -0500

    Include scale for final logits

--- modula/compound.py
+++ modula/compound.py
@@ -18,17 +18,17 @@ def Attention(num_heads, d_embed, d_query, d_value, softmax_scale, causal):
     AttentionScores = Softmax(softmax_scale) @ CausalMask() @ AttentionQK() @ Rope(d_query) @ (Q, K)
     return W @ (1/3 * AttentionOutput()) @ (V, AttentionScores)
 
-def GPT(vocab_size, num_heads, d_embed, d_query, d_value, num_blocks, blocks_mass=5, softmax_scale=1.0):
+def GPT(vocab_size, num_heads, d_embed, d_query, d_value, num_blocks, blocks_mass=5, attention_scale=1.0, final_scale=1.0):
     embed = Embed(d_embed, vocab_size)
     embed.tare()
 
-    att = Attention(num_heads, d_embed, d_query, d_value, softmax_scale, causal=True)
+    att = Attention(num_heads, d_embed, d_query, d_value, attention_scale, causal=True)
     mlp = Linear(d_embed, 4*d_embed) @ GeLU() @ Linear(4*d_embed, d_embed)
     att_block = (1-1/(2*num_blocks)) * Identity() + 1/(2*num_blocks) * att
     mlp_block = (1-1/(2*num_blocks)) * Identity() + 1/(2*num_blocks) * mlp
     blocks = (mlp_block @ att_block) ** num_blocks
     blocks.tare(absolute=blocks_mass)
 
-    out = Linear(vocab_size, d_embed)
+    out = final_scale * Linear(vocab_size, d_embed)
 
     return out @ blocks @ embed
